023e5a9b67be6b256ef19d0ea9c85584ef10267a
akshayadupati <akshaya.dupati@gmail.com>
2022-03-25 04:16:30 +0530

    Added search bar and added list and show fucntionality in controller

---
+++ CumulativeProject1-AkshayaDupati/Controllers/TeacherController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CumulativeProject1_AkshayaDupati.Models;
+
+namespace CumulativeProject1_AkshayaDupati.Controllers
+{
+    public class TeacherController : Controller
+    {
+        // GET: Teacher
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET : Teacher/List/{SearchKey}
+        [Route("Teacher/List/{SearchKey}")]
+
+        /// <summary>
+        /// Controller that fetches list of teachers from TeacherDataController
+        /// </summary>
+        /// <param name="SearchKey"></param>
+        /// <returns>Returns the list of teachers to the view.</returns>
+        public ActionResult List(string SearchKey)
+        {
+            TeacherDataController controller = new TeacherDataController();
+            IEnumerable<Teacher> Teachers= controller.ListTeachers(SearchKey);
+
+            return View(Teachers);
+        }
+
+        // GET : Teacher/Show/{id}
+
+        /// <summary>
+        /// Controller that fetches particular data typed in the URL from the 
+        /// TeacherDataController
+        /// </summary>
+        /// <returns>Returns the particular teacher data to the view.</returns>
+        public ActionResult Show(int id)
+        {
+
+            TeacherDataController controller = new TeacherDataController();
+            Teacher SelectedTeacher = controller.FindTeacher(id);
+
+            return View(SelectedTeacher); 
+        }
+    }
+}
--- CumulativeProject1-AkshayaDupati/Controllers/TeacherDataController.cs
+++ CumulativeProject1-AkshayaDupati/Controllers/TeacherDataController.cs
@@ -6,6 +6,7 @@ using System.Net.Http;
 using System.Web.Http;
 using MySql.Data.MySqlClient;
 using CumulativeProject1_AkshayaDupati.Models;
+using System.Diagnostics;
 
 namespace CumulativeProject1_AkshayaDupati.Controllers
 {
@@ -13,31 +14,104 @@ namespace CumulativeProject1_AkshayaDupati.Controllers
     {
         private SchoolDbContext School = new SchoolDbContext();
 
+        /// <summary>
+        /// The ListTeachers function returns list of teachers
+        /// </summary>
+        /// <param name="SearchKey">Search key that defines teacher name . Optional parameter.</param>
+        /// <example>GET : /api/TeacherData/ListTeachers</example>
+        /// <example>GET : /api/TeacherData/ListTeachers/Akshaya</example>
+        /// <returns>
+        /// A list of teachers with teacherid, teacherfname and teacherlname
+        /// </returns>
+
         [HttpGet]
-        public IEnumerable<string> ListTeachers()
+        [Route("api/TeacherData/ListTeachers/{SearchKey?}")]
+        public List<Teacher> ListTeachers(string SearchKey = null)
         {
+
+            if (SearchKey != null)
+            {
+                Debug.WriteLine("Hey there! Search key is present! ");
+            }
+
             MySqlConnection Conn = School.AccessDatabase();
 
             Conn.Open();
 
             MySqlCommand cmd = Conn.CreateCommand();
 
-            cmd.CommandText = "SELECT * FROM TEACHERS";
+            string query = "SELECT * FROM TEACHERS";
+
+            if(SearchKey != null)
+            {
+                query = query + " where lower(teacherfname) = lower(@key)";
+                cmd.Parameters.AddWithValue("@key", SearchKey);
+                cmd.Prepare();
+            }
+
+            cmd.CommandText = query;
 
             MySqlDataReader ResultSet = cmd.ExecuteReader();
 
-            List<string> TeacherNames = new List<string> { };
+            List<Teacher> Teachers = new List<Teacher> { };
 
             while (ResultSet.Read())
             {
-                string TeacherName = ResultSet["teacherfname"] + " " + ResultSet["teacherlname"];
 
-                TeacherNames.Add(TeacherName);
+                Teacher NewTeacher = new Teacher();
+
+                NewTeacher.TeacherId = Convert.ToInt32(ResultSet["teacherid"]);
+                NewTeacher.TeacherFName = ResultSet["teacherfname"].ToString();
+                NewTeacher.TeacherLName = ResultSet["teacherlname"].ToString();
+
+                Teachers.Add(NewTeacher);
             }
 
             Conn.Close();
 
-            return TeacherNames;
+            return Teachers;
+        }
+
+        /// <summary>
+        /// The FindTeacher function returns data of that particular teacher id.
+        /// </summary>
+        /// <example>GET : api/TeacherData/FindTeacher/{3}</example>
+        /// <returns>
+        /// Returns teacherid, teacherfname and teacherlname of 3rd teacher in the database.
+        /// </returns>
+
+        [HttpGet]
+        [Route("api/TeacherData/FindTeacher/{teacherId}")]
+
+        public Teacher FindTeacher(int teacherId)
+        {
+
+            MySqlConnection Conn = School.AccessDatabase();
+
+            Conn.Open();
+
+            MySqlCommand cmd = Conn.CreateCommand();
+
+            cmd.CommandText = "select * from teachers where teacherid=@id";
+
+            cmd.Parameters.AddWithValue("@id", teacherId);
+
+            cmd.Prepare();
+
+            MySqlDataReader ResultSet = cmd.ExecuteReader();
+
+            Teacher selectedTeacher = new Teacher();
+
+            while (ResultSet.Read())
+            {
+                selectedTeacher.TeacherId = Convert.ToInt32(ResultSet["teacherid"]);
+                selectedTeacher.TeacherFName = ResultSet["teacherfname"].ToString();
+                selectedTeacher.TeacherLName = ResultSet["teacherlname"].ToString();
+            }
+
+            Conn.Close();
+
+            return selectedTeacher;
         }
     }
 }
